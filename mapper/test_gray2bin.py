115ab6873a2f0dafa7934901a2b745cb2999f51a
Anton Urvantsev <urvantsev.ant@gmail.com>
2023-01-15 21:33:12 +0100

    Revert "Revert "init commit"" (#3)

---
+++ mapper/test_gray2bin.py
@@ -0,0 +1,19 @@
+import cocotb
+from cocotb.triggers import Timer
+
+
+@cocotb.test()
+async def my_first_test(dut):
+    """Try accessing the design."""
+
+    dut.rst = 1
+    dut.i_gray_data.value = 45
+
+    for cycle in range(10):
+        dut.clk.value = 0
+        await Timer(1, units="ns")
+        dut.clk.value = 1
+        await Timer(1, units="ns")
+
+    dut._log.info("i_data is %s", dut.i_data.value)
+    assert dut.i_data.value == 0, "i_data.value is not 0!"
