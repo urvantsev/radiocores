50951d2d759cbf74c450a585b2dbf059eb65133d
Anton Urvantsev <urvantsev.ant@gmail.com>
2024-11-25 18:18:21 +0000

    refactor(gray2bin): move diagram, rename test
    Moved the diagram file to the gray2bin/doc directory.
    Renamed the test to allow separation of simulation results.

--- radiocores/gray2bin/rtl/test_gray2bin.py
+++ radiocores/gray2bin/rtl/test_gray2bin.py
@@ -3,20 +3,27 @@ import pytest
 from cocotb.runner import get_runner
 from pathlib import Path
 
-# Define the project path and sources
-proj_path = Path(__file__).resolve().parent
+# Current working directory, should be root of the repository
+cwd = Path.cwd()
+
+# Define rtl sources
+rtl_path = cwd / "radiocores" / "gray2bin" / "rtl"
+
 sources = [
-    proj_path / "gray2bin_if.svh",
-    proj_path / "node_if.svh",
-    proj_path / "node.sv",
-    proj_path / "gray2bin.sv",
-    proj_path / "gray2bin_wrapper.sv",
+    rtl_path / "node.sv",
+    rtl_path / "gray2bin.sv",
+    rtl_path / "gray2bin_wrapper.sv",
+]
+
+# Define includes
+includes = [
+    cwd / "radiocores" / "include",
 ]
 
 
 @pytest.mark.parametrize("test_module", ["tests.tb_gray2bin"])
 def test_cocotb_simulation(test_module):
-    build_dir = proj_path / f"sim_build_{test_module.split('.')[-1]}"
+    build_dir = rtl_path / f"sim_build_{test_module.split('.')[-1]}"
     vcd_file = build_dir / f"{test_module.split('.')[-1]}.vcd"
 
     os.environ["COCOTB_RESULTS_FILE"] = str(vcd_file)
@@ -25,12 +32,13 @@ def test_cocotb_simulation(test_module):
     runner = get_runner("verilator")
 
     # Add the include paths
-    include_dirs = [proj_path]
+    include_dirs = [rtl_path]
     include_args = [f"-I{include_dir}" for include_dir in include_dirs]
 
     # Build the simulation
     runner.build(
         verilog_sources=sources,
+        includes=includes,
         hdl_toplevel="gray2bin_wrapper",
         build_dir=build_dir,
         build_args=["--trace", "--trace-structs", "--coverage"] + include_args,
